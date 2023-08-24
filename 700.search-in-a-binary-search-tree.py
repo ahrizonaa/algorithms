7a8a238607ccc2a79c4332edd84f12311a3d8e43
missahrizona <princessssahri@gmail.com>
2023-08-24 00:38:50 -0500

    dfs find val in bst

---
+++ 700.search-in-a-binary-search-tree.py
@@ -0,0 +1,28 @@
+#
+# @lc app=leetcode id=700 lang=python3
+#
+# [700] Search in a Binary Search Tree
+#
+
+# @lc code=start
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def searchBST(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
+        def dfs(node):
+            if not node:
+                return None
+            if node.val==val:
+                return node
+            
+            if val < node.val:
+                return dfs(node.left)
+            return dfs(node.right)
+        
+        return dfs(root)
+# @lc code=end
+
