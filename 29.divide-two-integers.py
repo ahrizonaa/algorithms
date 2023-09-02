2a4ef357e56f7745d6aa12c6e557d0cd3bbab9ce
missahrizona <princessssahri@gmail.com>
2023-09-02 00:44:37 -0500

    exponential search divide 2 integers without using * or / or % operators

--- 29.divide-two-integers.py
+++ 29.divide-two-integers.py
@@ -7,71 +7,26 @@
 
 # @lc code=start
 class Solution:
-    def signer(self, n, dividend, divisor):
-        if dividend < 0 and divisor < 0:
-            return -n
-        if dividend > 0 and divisor > 0:
-            return n
-        return -n
-
-    def limiter(self, dividend):
-        # print("limiter", dividend)
-        if dividend < self.lower:
-            return self.lower
-        elif dividend > self.upper:
-            return self.upper
-        return dividend
-
     def divide(self, dividend: int, divisor: int) -> int:
-        if dividend == 0:
-            return 0
+        if dividend == -(2 << 30) and divisor == -1:
+            return (2 << 30) - 1
 
         positive = True if dividend >> 31 == divisor >> 31 else False
 
-        if abs(dividend) == abs(divisor):
-            if positive:
-                return 1
-            return -1
+        divisor = -divisor if divisor < 0 else divisor
+        dividend = -dividend if dividend < 0 else dividend
 
-        self.lower = -(2 << 30)
-        self.upper = -self.lower - 1
+        ans = 0
+        while dividend >= divisor:
+            i = 1
+            curr = divisor
+            while curr + curr <= dividend:
+                curr = curr + curr
+                i += i
+            ans += i
+            dividend = dividend - curr
 
-        if positive:
-            dividend = abs(dividend)
-            divisor = abs(divisor)
-
-        # print("here")
-        if abs(divisor) == 1:
-            # print(positive)
-            if positive and dividend < 0:
-                dividend = -dividend
-                # print("44", dividend)
-            return self.limiter(dividend)
-
-        if dividend < self.lower:
-            return self.lower
-        if dividend > self.upper:
-            return self.upper
-
-        curr = abs(divisor)
-        i = 1
-        # print("while")
-        while curr < abs(dividend):
-            curr = curr << 1
-            i += 1
-            print(i, curr)
-
-        i -= 2
-        ans = (2 << i) - abs(divisor)
-        return ans
-        # return self.signer(ans, dividend, divisor)
+        return ans if positive else -ans
 
 
 # @lc code=end
-print("ans", Solution().divide(2147483647, 2))
-
-# 10
-# 3
-# 7
-# -3
-5
