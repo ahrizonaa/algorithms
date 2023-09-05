27a2ecbfd1f14b33889a5e6cabf5f285c559176c
missahrizona <princessssahri@gmail.com>
2023-09-04 23:11:12 -0500

    built-in sort with lambda using pos or neg constant as key

---
+++ 283.move-zeroes.py
@@ -0,0 +1,17 @@
+#
+# @lc app=leetcode id=283 lang=python3
+#
+# [283] Move Zeroes
+#
+
+
+# @lc code=start
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        nums.sort(key=lambda n: -1 if not n else 1, reverse=True)
+
+
+# @lc code=end
